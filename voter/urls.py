5f4f024968c1c631544ff22c346f7618ecfb1039
Illia Polosukhin <ilblackdragon@gmail.com>
2013-07-23 20:02:00 -0700

    Update to support Django 1.6 urls

--- voter/urls.py
+++ voter/urls.py
@@ -1,6 +1,10 @@
-from django.conf.urls.defaults import patterns, url
+try:
+    from django.conf.urls import patterns, url
+except ImportError:
+    from django.conf.urls.defaults import patterns, url # Django < 1.4
 
 urlpatterns = patterns('voter.views',
     url(r'^like/(?P<obj_type>[\w]+)/(?P<obj_id>[\d]+)/$', 'set_like', name='ratings_like'),
     url(r'^dislike/(?P<obj_type>[\w]+)/(?P<obj_id>[\d]+)/$', 'set_dislike', name='ratings_dislike'),
 )
+
